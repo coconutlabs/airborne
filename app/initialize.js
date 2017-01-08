f9462f0f2c26777aad43b24f57b715365fce8786
Jonathan Bender <jlbender@gmail.com>
2017-01-08 18:19:59 -0500

    add caching to track intermittent updates

--- app/initialize.js
+++ app/initialize.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
+	const _ = require('lodash')
 	const moment = require('moment')
 	const leaflet = require('leaflet')
 
@@ -9,35 +10,55 @@ document.addEventListener('DOMContentLoaded', () => {
 	const AirborneFlightMarker = require('./objects/flight_marker')
 
 	const appMap = new AirborneMap('app-map')
-	let plottedFlights = new leaflet.FeatureGroup()
+
+	let flightLayer = new leaflet.FeatureGroup()
+	appMap.addLayer(flightLayer)
+
+	const plottedFlights = {}
 
 	const plotFlights = () => {
-		const newFlightLayer = new leaflet.FeatureGroup()
-
 		FlightData.getAll((response) => {
-			const dataTimestamp = moment.unix(response.time).format('MMMM Do YYYY, h:mm:ss a')
+			const dataTimestamp = moment.unix(response.time).format()
 			console.log(`${response.states.length} flights updated ${dataTimestamp}`)
 
 			let plotCount = 0
 
 			for (let i = 0; i < response.states.length; i++) {
 				let flight = new AirborneFlight(response.states[i])
-				if (!flight.isPlottable || flight.onGround) { continue; }
+				let plottedFlight = plottedFlights[flight.icao24]
+
+				if (!flight.isPlottable || flight.onGround) {
+					if (plottedFlight) {
+						flightLayer.removeLayer(plottedFlight.marker)
+						delete plottedFlights[flight.icao24]
+					}
+					continue;
+				}
 
 				let marker = new AirborneFlightMarker(flight)
 
-				newFlightLayer.addLayer(marker)
+				if (plottedFlight) flightLayer.removeLayer(plottedFlight.marker)
+				flightLayer.addLayer(marker)
+
+				plottedFlights[flight.icao24] = { flight, marker }
 
 				plotCount++
 			}
 
-			appMap.removeLayer(plottedFlights)
-			appMap.addLayer(newFlightLayer)
-			plottedFlights = newFlightLayer
-
 			console.log(`Plotted ${plotCount} flights`);
 
+			cleanUpOldData()
+
 			setTimeout(plotFlights, 10000)
+		})
+	}
+
+	const cleanUpOldData = () => {
+		const timeCutoff = moment().subtract(5, 'minutes').unix()
+
+		_.each(plottedFlights, (data, identifier) => {
+			const { flight } = data
+			if (flight.timePosition < timeCutoff) { delete plottedFlights[identifier] }
 		})
 	}
 
